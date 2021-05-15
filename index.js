81135d0ca9cc6c679f58b97a7de305cd3c632d18
sauravsh28 <35374118+sauravsh28@users.noreply.github.com>
2021-05-15 19:28:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -76,6 +76,9 @@ app.get("/user", requiresAuth(), async (req, res) => {
 });
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
   try {
+     // 👇 get the token from the request 👇
+    const { token_type, access_token } = req.oidc.accessToken;
+    // 👇 then send it as an authorization header 👇
     const expenses = await axios.get(`${API_URL}/reports`);
     res.render("expenses", {
       user: req.oidc && req.oidc.user,
